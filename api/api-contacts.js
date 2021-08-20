b32704dc5608e3690689e2f8360e4de6bf702507
Artem Smirnov <aratatem2013@gmail>
2021-08-20 14:56:07 +0300

    little fix

--- api/api-contacts.js
+++ api/api-contacts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { contacts: ctrl } = require("../controllers");
+
 const { addContactValidation, updateContactValidation } = require("./validation");
 
 router.get("/", ctrl.getAll);
--- model/index.js
+++ model/index.js
@@ -2,10 +2,10 @@ const fs = require("fs/promises");
 const path = require("path");
 const { v4: uuid } = require("uuid");
 
-const contacsDataPath = path.join(__dirname, "./contacts.json");
+const contactsDataPath = path.join(__dirname, "./contacts.json");
 
 const listContacts = async () => {
-  const data = await fs.readFile(contacsDataPath, "utf-8");
+  const data = await fs.readFile(contactsDataPath, "utf-8");
   return JSON.parse(data);
 };
 
@@ -20,7 +20,7 @@ const removeContact = async (contactId) => {
   const index = data.findIndex(({ id }) => id === contactId);
   if (index !== -1) {
     const newData = data.splice(index, 1);
-    await fs.writeFile(contacsDataPath, JSON.stringify(data, null, " "));
+    await fs.writeFile(contactsDataPath, JSON.stringify(data, null, " "));
     return newData;
   }
   return null;
